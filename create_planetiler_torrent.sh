71f150b51f0168a3c0e909f623678f1b408f49bd
Andrew Calcutt <acalcutt@techidiots.net>
2023-09-10 17:09:07 -0400

    rework for planetiler generation

--- create_planetiler_torrent.sh
+++ create_planetiler_torrent.sh
@@ -1,61 +1,71 @@
 #!/bin/bash
 
-# DO NOT EDIT - This file is being maintained by Chef
-
 # Exit on error
 set -e
 
-# Get the name of the file and the expected pattern
-file="$1"
-pattern="^osm-([0-9]{4})-([0-9]{2})-([0-9]{2})\.dmp$"
-
-# Give up now if the file isn't a database dump
-[[ $file =~ $pattern ]] || exit 0
-
-# Save the year and date from the file name
-year="${BASH_REMATCH[1]}"
-date="${year:2:2}${BASH_REMATCH[2]}${BASH_REMATCH[3]}"
+year="$(date +'%Y')"
+date="$(date +'%Y%m%d')"
 
 # Check the lock
-if [ -f /tmp/planetdump.lock ]; then
-    if [ "$(ps -p `cat /tmp/planetdump.lock` | wc -l)" -gt 1 ]; then
-        echo "Error: Another planetdump is running"
+if [ -f /tmp/planetilerdump.lock ]; then
+    if [ "$(ps -p `cat /tmp/planetilerdump.lock` | wc -l)" -gt 1 ]; then
+        echo "Error: Another planetilerdump is running"
         exit 1
     else
-        rm /tmp/planetdump.lock
+        rm /tmp/planetilerdump.lock
     fi
 fi
 
+# Redirect this shell's output to a file. This is so that it
+# can be emailed later, since this script is run from incron
+# and incron doesn't yet support MAILTO like cron does. The
+# command below appears to work in bash as well as dash.
+logfile="/tmp/planetilerdump.log.$$"
+exec > "${logfile}" 2>&1
+
 # Create lock file
-echo $$ > /tmp/planetdump.lock
+echo $$ > /tmp/planetilerdump.lock
 
 # Define cleanup function
 function cleanup {
     # Remove the lock file
-    rm /tmp/planetdump.lock
+    rm /tmp/planetilerdump.lock
+
+    # Send an email with the output, since incron doesn't yet
+    # support doing this in the incrontab
+    if [[ -s "$logfile" ]]
+    then
+        mailx -s "Planetiler output: ${file}" acalcutt@techidiots.net < "${logfile}"
+    fi
+
+    # Remove the log file
+    rm -f "${logfile}"
 }
 
 # Remove lock on exit
 trap cleanup EXIT
 
 # Change to working directory
-cd /store/planetdump
+cd /store/planetilerdump
+
+# Make directory to put outpul files
+mkdir -p $year
 
 # Cleanup
-rm -rf users
-rm -rf changesets changeset_tags changeset_comments
-rm -rf nodes node_tags
-rm -rf ways way_tags way_nodes
-rm -rf relations relation_tags relation_members
+rm -rf data
 
 # Run the dump
-time nice -n 19 /opt/planet-dump-ng/planet-dump-ng \
-     --max-concurrency=4 \
-     -c "pbzip2 -c" -f "/store/backup/${file}" --dense-nodes=1 \
-     -C "changesets-${date}.osm.bz2" \
-     -D "discussions-${date}.osm.bz2" \
-     -x "planet-${date}.osm.bz2" -X "history-${date}.osm.bz2" \
-     -p "planet-${date}.osm.pbf" -P "history-${date}.osm.pbf"
+time java -Xmx25g \
+  -jar /opt/planetiler/planetiler_0.6.0.jar \
+  `# Download the latest planet.osm.pbf from s3://osm-pds bucket` \
+  --area=planet --bounds=planet --download \
+  `# Accelerate the download by fetching the 10 1GB chunks at a time in parallel` \
+  --download-threads=10 --download-chunk-size-mb=1000 \
+  `# Also download name translations from wikidata` \
+  --fetch-wikidata \
+  --output=planetiler-$date.mbtiles \
+  `# Store temporary node locations at fixed positions in a memory-mapped file` \
+  --nodemap-type=array --storage=mmap
 
 # Function to create bittorrent files
 function mk_torrent {
@@ -64,9 +74,9 @@ function mk_torrent {
   dir="$3"
   s_year="$4"
   web_dir="${dir}${s_year}"
-  name="${type}-${date}.osm.${format}"
+  name="${type}-${date}.${format}"
   web_path="${web_dir}/${name}"
-  rss_web_dir="https://planet.openstreetmap.org/${dir}"
+  rss_web_dir="https://planetiler.techidiots.net/${dir}"
   rss_file="${type}-${format}-rss.xml"
   torrent_file="${name}.torrent"
   torrent_url="${rss_web_dir}${s_year}/${torrent_file}"
@@ -78,16 +88,8 @@ function mk_torrent {
      -a udp://tracker.torrent.eu.org:451 \
      -a udp://tracker-udp.gbitt.info:80/announce,http://tracker.gbitt.info/announce,https://tracker.gbitt.info/announce \
      -a http://retracker.local/announce \
-     -w "https://planet.openstreetmap.org/${web_path}" \
-     -w "https://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/${web_path}" \
-     -w "https://ftpmirror.your.org/pub/openstreetmap/${web_path}" \
-     -w "https://mirror.init7.net/openstreetmap/${web_path}" \
-     -w "https://ftp.fau.de/osm-planet/${web_path}" \
-     -w "https://ftp.spline.de/pub/openstreetmap/${web_path}" \
-     -w "https://downloads.opencagedata.com/planet/${name}" \
-     -w "https://planet.osm-hr.org/${web_path}" \
-     -w "https://planet.maps.mail.ru/${web_path}" \
-     -c "OpenStreetMap ${type} data export, licensed under https://opendatacommons.org/licenses/odbl/ by OpenStreetMap contributors" \
+     -w "https://planetiler.techidiots.net/${web_path}" \
+     -c "Planetiler ${type} data export ${name}" \
      -o "${torrent_file}" > /dev/null
 
   # create .xml global RSS headers if missing
@@ -103,16 +105,16 @@ function mk_torrent {
 		--attr "version" --output "2.0" --break \
 		--attr "atom:DUMMY" --break \
 	--elem "channel" \
-	--elem "title" --output "OpenStreetMap ${type} ${format} torrent RSS" --break \
+	--elem "title" --output "Planetiler ${type} ${format} torrent RSS" --break \
 	--elem "link"  --output "${rss_web_dir}" --break \
 	--elem "atom:link" \
 		--attr "href" --output "${rss_web_dir}/${rss_file}" --break \
 		--attr "rel" --output "self" --break \
 		--attr "type" --output "application/rss+xml" --break \
 		--break \
-	--elem "description" --output "${type}.osm.${format}.torrent RSS feed" --break \
-	--elem "copyright" --output "Source: OpenStreetMap contributors, under ODbL 1.0 licence" --break \
-	--elem "generator" --output "OpenStreetMap xmlstarlet powered shell script v1.0" --break \
+	--elem "description" --output "${type}.${format}.torrent RSS feed" --break \
+	--elem "copyright" --output "Source: OpenStreetMap contributors, under ODbL 1.0 licence. OpenMapTiles under BSD 3-Clause License/CC-BY 4.0" --break \
+	--elem "generator" --output "planetiler shell script v1.0" --break \
 	--elem "language" --output "en" --break \
 	--elem "lastBuildDate" --output "${torrent_time_rfc}" \
 	> "${rss_file}"
@@ -125,12 +127,12 @@ function mk_torrent {
 	-s "//item[1]" -t elem -n "guid" -v "${torrent_url}" \
 	-s "//item[1]" -t elem -n "link" -v "${torrent_url}" \
 	-s "//item[1]" -t elem -n "pubDate" -v "${torrent_time_rfc}" \
-	-s "//item[1]" -t elem -n "category" -v "OpenStreetMap data" \
+	-s "//item[1]" -t elem -n "category" -v "Planetiler data" \
 	-s "//item[1]" -t elem -n "enclosure" \
 		-s "//item[1]"/enclosure -t attr -n "type" -v "application/x-bittorrent" \
 		-s "//item[1]"/enclosure -t attr -n "length" -v "${torrent_size}" \
 		-s "//item[1]"/enclosure -t attr -n "url" -v "${torrent_url}" \
-	-s "//item[1]" -t elem -n "description" -v "OpenStreetMap torrent ${torrent_file}" \
+	-s "//item[1]" -t elem -n "description" -v "Planetiler torrent ${torrent_file}" \
 	-u /rss/channel/lastBuildDate -v "${torrent_time_rfc}" \
 	-d /rss/@atom:DUMMY \
 	-d "//item[position()>5]" \
@@ -143,8 +145,8 @@ function install_dump {
   format="$2"
   dir="$3"
   year="$4"
-  name="${type}-${date}.osm.${format}"
-  latest="${type}-latest.osm.${format}"
+  name="${type}-${date}.${format}"
+  latest="${type}-latest.${format}"
   rss_file="${type}-${format}-rss.xml"
 
   md5sum "${name}" > "${name}.md5"
@@ -158,17 +160,19 @@ function install_dump {
 }
 
 # Create *.torrent files
-mk_torrent "changesets" "bz2" "planet" "/${year}"
-mk_torrent "discussions" "bz2" "planet" "/${year}"
-mk_torrent "planet" "bz2" "planet" "/${year}"
-mk_torrent "history" "bz2" "planet/full-history" "/${year}"
-mk_torrent "planet" "pbf" "pbf"
-mk_torrent "history" "pbf" "pbf/full-history"
+
+mk_torrent "planetiler" "mbtiles" "planetiler" "${year}"
 
 # Move dumps into place
-install_dump "changesets" "bz2" "<%= node[:planet][:dump][:xml_directory] %>" "${year}"
-install_dump "discussions" "bz2" "<%= node[:planet][:dump][:xml_directory] %>" "${year}"
-install_dump "planet" "bz2" "<%= node[:planet][:dump][:xml_directory] %>" "${year}"
-install_dump "history" "bz2" "<%= node[:planet][:dump][:xml_history_directory] %>" "${year}"
-install_dump "planet" "pbf" "<%= node[:planet][:dump][:pbf_directory] %>"
-install_dump "history" "pbf" "<%= node[:planet][:dump][:pbf_history_directory] %>"
+install_dump "planetiler" "mbtiles" "/store/http/" "${year}"
+
+# Remove pbf dumps older than 90 days
+find /store/http/ \
+     -maxdepth 1 -mindepth 1 -type f -mtime +90 \
+     \( \
+     -iname 'planetiler-*.mbtiles' \
+     -o -iname 'planetiler-*.mbtiles.md5' \
+     -o -iname 'planetiler-*.pmtiles' \
+     -o -iname 'planetiler-*.pmtiles.md5' \
+     \) \
+     -delete
